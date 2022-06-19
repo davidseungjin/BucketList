2acf65b10c5155cab292401dd371ab9fe63e2c9d
David Lee <david.lee.seungjin@gmail.com>
2022-06-19 15:49:29 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by David Lee on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
